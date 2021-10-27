6cc41f97d25f9e6ff4839895b742e397d12eded0
Lucille Huang <lucillejyhuang@gmail.com>
2021-10-27 09:55:02 -0400

    A2 task2 sql code

---
+++ Assignment 2/task1.sql
@@ -0,0 +1,34 @@
+-- Q1 start
+SELECT DISTINCT penaltySeverity
+FROM GamePenalties;
+-- Q1 end
+
+-- Q2 start
+SELECT COUNT(DISTINCT playType)
+FROM GamePlays;
+-- Q2 end
+
+-- Q3 start
+WITH PbT AS 
+	(SELECT COUNT(playID) AS numPlays
+		FROM GamePlays
+		GROUP BY playType)
+SELECT playType
+	FROM PbT
+	WHERE numPlays =
+	(SELECT MAX(numPlays)
+		FROM PbT);
+-- Q3 end
+
+--Q4 start
+WITH PbT AS 
+	(SELECT COUNT(playID) AS numPlays
+		FROM GamePlays INNER JOIN Game USING(gameID)
+		WHERE gameType = 'P'
+		GROUP BY playType)
+SELECT playType
+	FROM PbT
+	WHERE numPlays =
+	(SELECT MAX(numPlays)
+		FROM PbT);
+-- Q4 end
