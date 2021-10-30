34dab23e39fe95eff225c70572c7800c49269e37
Lucille Huang <lucillejyhuang@gmail.com>
2021-10-30 12:44:32 -0400

    Create Tables, alter schema

--- Assignment2/task2.sql
+++ Assignment2/task2.sql
@@ -1,28 +1,22 @@
+drop table if exists BetterGameGoals;
+drop table if exists BetterGamePlays;
+
 -- create the tables BetterGamePlays & BetterGameGoals
-CREATE TABLE IF NOT EXISTS BetterGamePlays LIKE GamePlays; --contain both playID and gameID
-CREATE TABLE IF NOT EXISTS BetterGameGoals LIKE GameGoals; --contain only playID
+CREATE TABLE BetterGamePlays LIKE GamePlays; --contain both playID and gameID
+CREATE TABLE BetterGameGoals LIKE GameGoals; --contain only playID
 
--- Modify these tables by using “ALTER TABLE ... ADD COLUMN”
--- to add the attributes
--- – gameID (if necessary)
--- – playNumber
-ALTER TABLE GamePlays (
-ADD COLUMN playNumber  column_definition
-        [FIRST | AFTER col_name]
-  | ADD [COLUMN] (col_name column_definition,...)
+-- Modify these tables to add the attributes
+-- • gameID (if necessary)
+-- • playNumber
+ALTER TABLE BetterGamePlays ADD COLUMN playNumber DECIMAL(3) AFTER gameID;
+ALTER TABLE BetterGameGoals ADD COLUMN gameID DECIMAL(10) FIRST,
+                            ADD COLUMN playNumber DECIMAL(3) AFTER gameID;
 
-ALTER TABLE tbl_name (
-ADD [COLUMN] col_name column_definition
-        [FIRST | AFTER col_name]
-  | ADD [COLUMN] (col_name column_definition,...)
+-- Modify these tables a second time by using “ALTER TABLE ... DROP COLUMN” to remove the playID attribute
+ALTER TABLE BetterGamePlays DROP COLUMN playID;
+ALTER TABLE BetterGameGoals DROP COLUMN playID;
 
--- • Modify these tables a second time by using “ALTER TABLE ... DROP
--- COLUMN” to remove the playID attribute
--- • Populate these new tables from the original tables, GamePlays and
--- GameGoals
--- You will need to use regexp in this operation.
--- • Get rid of the original tables, GamePlays and GameGoals by using
--- “DROP TABLE”
--- • Create views GamePlays and GameGoals, based on BetterGame-
--- Plays and BetterGameGoals, that have an identical schema to the
--- original GamePlays and GameGoals tables.
+-- Populate these new tables from the original tables, GamePlays and GameGoals. You will need to use regexp in this operation.
+
+-- • Get rid of the original tables, GamePlays and GameGoals by using “DROP TABLE”
+-- • Create views GamePlays and GameGoals, based on BetterGamePlays and BetterGameGoals, that have an identical schema to the original GamePlays and GameGoals tables
